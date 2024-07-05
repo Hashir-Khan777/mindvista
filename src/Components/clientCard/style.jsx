2cd67ff2a5808285e29d8e69e9108730c6c1b7e7
Hania Imran <hanna.khan205@gmail.com>
2024-07-05 20:20:38 +0500

    fixes

--- src/Components/clientCard/style.jsx
+++ src/Components/clientCard/style.jsx
@@ -2,8 +2,9 @@ const style = {
   reviewBox: {
     w: { base: "300px", sm: "380px", lg: "300px", xl: "380px" },
     p: { base: "20px   ", sm: "40px", lg: "20px", xl: "40px" },
-    borderRadius: "30px",
-    border: "2px solid #E7E5EA",
+    // borderRadius: "30px",
+    // border: "2px solid #E7E5EA",
+    bgColor:"#090909"
   },
   text: {
     mt: "30px",
