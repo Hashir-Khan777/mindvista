14df3580c6e970f77338607b174fcd0c29abd9c2
Hania Imran <hanna.khan205@gmail.com>
2024-07-09 22:26:51 +0500

    fixes

--- src/Pages/portfolioDetails/index.jsx
+++ src/Pages/portfolioDetails/index.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link as RouterLink } from "react-router-dom";
 import {
   Box,
   Heading,
@@ -7,17 +7,27 @@ import {
   Stack,
   Tag,
   TagLabel,
+  SimpleGrid,
+  Button,
+  Card,
+  CardBody,
 } from "@chakra-ui/react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
 import { projectData, tagColors } from "../portfolio/PortfolioData";
+
 const ProjectDetails = () => {
   const { category, projectId } = useParams();
   const project = projectData[category][projectId];
 
+  // Fetch related projects excluding the current project
+  const relatedProjects = Object.keys(projectData[category])
+    .filter((id) => id !== projectId)
+    .map((id) => ({ id, ...projectData[category][id] }));
+
   return (
-    <Box p="4rem" bgColor="#090909" color="#fff">
-      <Stack direction={{ base: "column", lg: "row" }} spacing="4rem">
+    <Box bgColor="#090909" color="#fff">
+      <Stack direction={{ base: "column", lg: "row" }} spacing="4rem" p="4rem">
         <Box w={{ base: "100%", lg: "50%" }}>
           <Swiper spaceBetween={50} slidesPerView={1}>
             {project?.image.map((img, index) => (
@@ -32,7 +42,6 @@ const ProjectDetails = () => {
             justifyContent: "center",
             display: "flex",
             flexDir: "column",
-            
           }}
           w={{ base: "100%", lg: "50%" }}
         >
@@ -48,6 +57,64 @@ const ProjectDetails = () => {
           </Stack>
         </Box>
       </Stack>
+
+      <Box mt="4rem" bgColor="#000" p="4rem">
+        <Heading as="h1" fontSize="2.5rem" mb="2rem">
+          Related Projects
+        </Heading>
+        <SimpleGrid
+          my="20px"
+          mt="2rem"
+          columns={{ base: 1, md: 2, lg: 3, xl: 4 }}
+          spacing="30px"
+          justifyItems="center"
+          alignItems={"center"}
+          m="auto"
+          placeItems={"center"}
+          mx={"4rem"}
+        >
+          {relatedProjects.map((relatedProject, idx) => (
+            <Card maxW="sm" key={idx} minH={"450px"} bgColor={"#1E1E1E"}>
+              <CardBody>
+                <Image
+                  src={relatedProject.image[0]}
+                  alt={relatedProject.title}
+                  borderRadius="lg"
+                  h={"auto"}
+                  w={"100%"}
+                />
+                <Stack mt="6" spacing="3">
+                  <Heading size="1.2rem" color={"#fff"}>
+                    {relatedProject.title}
+                  </Heading>
+                  <Text color={"#fff"} fontSize={"0.8rem"}>
+                    {relatedProject.description}
+                  </Text>
+                  <Stack direction="row" spacing={2} mt={2}>
+                    {relatedProject.tags.map((tag, tagIdx) => (
+                      <Tag key={tagIdx} colorScheme={tagColors[tag]}>
+                        <TagLabel fontSize={"0.7rem"}>{tag}</TagLabel>
+                      </Tag>
+                    ))}
+                  </Stack>
+                  <Button
+                    as={RouterLink}
+                    to={`/project/${category}/${relatedProject.id}`}
+                    mt="1rem"
+                    sx={{
+                      fontSize: { base: "10px", md: "12px" },
+                      fontWeight: 700,
+                      mt: "10px",
+                    }}
+                  >
+                    View More Details
+                  </Button>
+                </Stack>
+              </CardBody>
+            </Card>
+          ))}
+        </SimpleGrid>
+      </Box>
     </Box>
   );
 };
