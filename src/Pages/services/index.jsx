4437b9437af2788c50d27684c25af4489605fc5b
Hania Imran <hanna.khan205@gmail.com>
2024-07-12 18:16:43 +0500

    fixes

--- src/Pages/services/index.jsx
+++ src/Pages/services/index.jsx
@@ -1,59 +1,116 @@
-// eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useState, useRef } from "react";
 import {
   Accordion,
   Box,
   Flex,
   Heading,
-  SimpleGrid,
   Grid,
   Stack,
   Text,
+  Image,
+  Button,
+  Tag,
 } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 import style from "./style";
-import Card from "../../Components/card";
 import CustomButton from "../../Components/button";
 import Customaccordian from "../../Components/customAccordian";
-import { GiMaterialsScience } from "react-icons/gi";
-import { motion } from "framer-motion";
-import ServiceCard from "../../Components/serviceCard";
+
+export const tagColors = {
+  HTML: "blue",
+  "Node.js": "green",
+  CSS: "green",
+  JavaScript: "orange",
+  "React Native": "cyan",
+  React: "purple",
+  Express: "red",
+  MongoDB: "teal",
+  Angular: "teal",
+  Dart: "yellow",
+  "SEO Audits": "pink",
+  Kotlin: "red",
+  "Link Building": "pink",
+  Room: "blue",
+  Swift: "gray",
+  "Keyword Research": "blue",
+  CoreData: "green",
+  Copywriting: "green",
+  Blogging: "orange",
+  "Smart Contracts": "orange",
+  AWS: "cyan",
+  "Reputation Management": "purple",
+  Solidity: "purple",
+  "PR Campaigns": "purple",
+  "Brand Strategy": "red",
+  Blender: "teal",
+  "Unreal Engine": "yellow",
+  Blueprint: "pink",
+  Photoshop: "cyan",
+  Godot: "purple",
+  IPFS: "purple",
+  GDScript: "yellow",
+  SEO: "gray",
+};
 
 const Services = () => {
   const servicesData = [
     {
       heading: "Web Designing",
-      text: "Crafting visually stunning and user-friendly websites that captivate and engage visitors. Our designs are tailored to reflect your brand identity while ensuring an optimal user experience.",
+      text: "Crafting visually stunning and user-friendly websites that captivate and engage visitors. Our designs are tailored to reflect your brand identity while ensuring an optimal user experience. C",
       img: "/images/web-design.png",
+      technologies: ["HTML", "CSS", "JavaScript", "React"],
     },
     {
       heading: "Web Development",
       text: "Building robust and scalable websites using the latest technologies to ensure optimal performance. Our development services include everything from custom website creation to e-commerce solutions and web applications.",
       img: "/images/web-dev.png",
+      technologies: ["Node.js", "Express", "MongoDB", "React"],
     },
     {
       heading: "App Development",
       text: "Developing intuitive and high-performing mobile applications for both iOS and Android platforms. We focus on creating apps that offer seamless functionality and an engaging user experience.",
       img: "/images/app-dev.png",
+      technologies: ["React Native", "Swift", "Kotlin"],
     },
     {
       heading: "Content Writing",
       text: "Producing compelling and relevant content that resonates with your target audience. Our content writing services cover everything from website copy to blog posts and social media content.",
       img: "/images/content-writing.png",
+      technologies: ["SEO", "Copywriting", "Blogging"],
     },
     {
       heading: "Branding and PR",
       text: "Build a strong brand identity and enhance your reputation with our branding and PR services. We help you define your brand story, manage your online reputation, and develop strategic PR campaigns to increase visibility and credibility.",
       img: "/images/branding-pr.png",
+      technologies: ["Brand Strategy", "PR Campaigns", "Reputation Management"],
     },
     {
       heading: "SEO Optimization",
       text: "Enhancing online visibility and search engine rankings to attract more organic traffic. Our SEO strategies are designed to improve your website's performance and help you reach a larger audience.",
       img: "/images/seo.png",
+      technologies: ["SEO Audits", "Keyword Research", "Link Building"],
     },
   ];
+
+  const [selectedService, setSelectedService] = useState(servicesData[0]);
+  const detailsRef = useRef(null);
+
+  const handleViewMore = (service) => {
+    setSelectedService(service);
+    detailsRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const renderTags = (technologies) => {
+    return technologies.map((tech) => (
+      <Tag key={tech} colorScheme={tagColors[tech]} mr="5px" mb="5px">
+        {tech}
+      </Tag>
+    ));
+  };
+
   return (
     <Box>
-      <Box py="4rem" pt={"8rem"} px="20px" textAlign="center" bgColor="#000">
+      <Box py="4rem" pt={"8rem"} textAlign="center" bgColor="#000">
         <Box m="auto" maxWidth={1440}>
           <Heading color="#F8BE28" sx={style.heading}>
             Services
@@ -64,13 +121,13 @@ const Services = () => {
           </Heading>
           <Box
             py="4rem"
-            pt={"8rem"}
-            px="20px"
+            mt={"4rem"}
+            // px="20px"
             textAlign="center"
-            bgColor="#000"
+            bgColor="#151515"
           >
             <Box my="3rem" textAlign="center">
-              <Text sx={style.subHeading}>our focusable services</Text>
+              <Text sx={style.subHeading}>Our focusable services</Text>
               <Heading sx={style.heading}>
                 How Do We Work To Help You In Startup Business
               </Heading>
@@ -85,10 +142,83 @@ const Services = () => {
                 gap={6}
               >
                 {servicesData.map((service, index) => (
-                  <ServiceCard key={index} service={service} />
+                  <Box
+                    key={index}
+                    as={motion.div}
+                    initial={{ opacity: 0, transform: "translateY(50px)" }}
+                    whileInView={{
+                      opacity: 1,
+                      transform: "translateY(0)",
+                      transition: { duration: 1 },
+                    }}
+                    sx={{
+                      ...style.services,
+                      borderColor:
+                        selectedService.heading === service.heading
+                          ? "#f8be28"
+                          : "transparent",
+                      borderWidth:
+                        selectedService.heading === service.heading
+                          ? "2px"
+                          : "0",
+                      boxShadow:
+                        selectedService.heading === service.heading
+                          ? "0 0 10px #f8be28"
+                          : "none",
+                    }}
+                  >
+                    <Box sx={style.serviceImgContainer}>
+                      <Image
+                        src={service.img}
+                        alt={service.heading}
+                        sx={style.serviceImg}
+                      />
+                    </Box>
+                    <Text sx={style.serviceHeading}>{service.heading}</Text>
+                    <Text sx={style.serviceText}>{service.text}</Text>
+                    <Button
+                      onClick={() => handleViewMore(service)}
+                      sx={{
+                        fontSize: { base: "10px", md: "12px" },
+                        fontWeight: 700,
+                        mt: "10px",
+                        color: "#f8be28",
+                        borderColor: "#f8be28",
+                        borderWidth: "1px",
+                        bgColor: "black",
+                        margin: "auto",
+                        display: "block",
+                        _hover: {
+                          color: "#090909",
+                          bgColor: "white",
+                        },
+                      }}
+                    >
+                      View More
+                    </Button>
+                  </Box>
                 ))}
               </Grid>
             </Box>
+          </Box>
+          <Box
+            ref={detailsRef}
+            bgColor="#000"
+            color="white"
+            textAlign={"left"}
+            maxW={{ base: "100%", md: "80%" }}
+            p={{ base: "1.5rem", md: "4rem" }}
+            m={"auto"}
+            // borderTop="2px solid #f8be28"
+          >
+            <Heading mb="2rem" color="#f8be28">
+              {selectedService.heading}
+            </Heading>
+            <Text mb="1rem">{selectedService.text}</Text>
+            <Text fontWeight="bold" mb="1rem">
+              Technologies Used:
+            </Text>
+            <Flex wrap="wrap">{renderTags(selectedService.technologies)}</Flex>
           </Box>
           <Stack
             placeItems="center"
--- src/Pages/services/style.jsx
+++ src/Pages/services/style.jsx
@@ -84,6 +84,46 @@ const style = {
     pt: { base: "2rem", lg: "4rem" },
     pb: "3rem",
   },
+  services: {
+    p: "2rem 2rem",
+    textAlign: "center",
+    justifyContent: "center",
+    display: "flex",
+    flexDirection: "column",
+    margin: "auto",
+    borderRadius: "8px",
+    boxShadow: "lg",
+  },
+  serviceImgContainer: {
+    height: { base: "60px", lg: "60px" },
+    width: { base: "80px", lg: "80px" },
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "0 auto 1rem",
+  },
+  serviceImg: {
+    maxHeight: "100%",
+    maxWidth: "100%",
+    objectFit: "contain",
+  },
+  serviceHeading: {
+    color: "#F8BE28",
+    fontSize: "16px",
+    fontWeight: 600,
+  },
+  serviceText: {
+    mt: "1rem",
+    mb: "2rem",
+    fontSize: { base: "12px", md: "14px" },
+    lineHeight: "1.5rem",
+    color: "white",
+    fontWeight: 400,
+  },
+  serviceContainer: {
+    p: { base: "1rem", lg: "2rem" },
+    // bgColor: "#151515",
+  },
 };
 
 export default style;
